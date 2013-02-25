9891af807807493bab59c016a10b9a5e7346e772
Per Thomas Lundal <perthomas@gmail.com>
2013-02-25 22:14:46 +0100

    Remove unneeded code and implement Øving 1

--- oeving2/oeving2.c
+++ oeving2/oeving2.c
@@ -5,65 +5,86 @@
  *****************************************************************************/
 
 #include "oeving2.h"
+#include "include/sys/interrupts.h"
 
-volatile avr32_pio_t *piob = &AVR32_PIOB;
-
+int current_button = ELEMENT_3;
 
 int main (int argc, char *argv[]) {
-  initHardware();
-
-  while(1);
-  return 0;
+    initHardware();
+    
+    while(1);
+    return 0;
 }
 
 /* funksjon for å initialisere maskinvaren, må utvides */
 void initHardware (void) {
-  initIntc();
-  initLeds();
-  initButtons();
-  initAudio();
+    initIntc();
+    initLeds();
+    initButtons();
+    //initAudio();
 }
 
 void initIntc(void) {
-  set_interrupts_base((void *)AVR32_INTC_ADDRESS);
-  init_interrupts();
+    set_interrupts_base((void *)AVR32_INTC_ADDRESS);
+    init_interrupts();
 }
 
 void initButtons(void) {
-  register_interrupt( button_isr, AVR32_PIOB_IRQ/32, AVR32_PIOB_IRQ % 32, BUTTONS_INT_LEVEL);
-  /* (...) */
+    register_interrupt( button_isr, AVR32_PIOB_IRQ/32, AVR32_PIOB_IRQ % 32, BUTTONS_INT_LEVEL);
+    piob->per = 0xFF;
+    piob->puer = 0xFF;
+    piob->ier = 0xFF;
+    /* (...) */
 }
 
 void initLeds(void) {
-  /* (...) */
+    pioc->per = 0xFF;
+    pioc->oer = 0xFF;
+    pioc->codr = 0xFF;
+    /* (...) */
 }
 
 void initAudio(void) {
-  register_interrupt( abdac_isr, AVR32_ABDAC_IRQ/32, AVR32_ABDAC_IRQ % 32, ABDAC_INT_LEVEL);
-  /* (...) */
+    register_interrupt( abdac_isr, AVR32_ABDAC_IRQ/32, AVR32_ABDAC_IRQ % 32, ABDAC_INT_LEVEL);
+    /* (...) */
 }
 
-void button_isr(void) {}
-
-void abdac_isr(void) {}
-
-__int_handler *button_handler(void)
-{
-	piob->per = ACTIVATED_BUTTONS; 
-	piob->puer = ACTIVATED_BUTTONS; 
-	//------enable button interrupts----
-	
-	
-	piob->ier = ACTIVATED_BUTTONS; 
-	piob->idr = ACTIVATED_BUTTONS; 
-	
+void button_isr(void) {
+    // Debounce
+    volatile int i = 1000;
+    while (i > 0) {
+        i--;
+    }
+    
+    //Read states
+    int event_states = piob->isr;
+    int button_states = ~piob->pdsr;
+    
+    int press = event_states & button_states;
+    
+    // Right press
+    if (press & ELEMENT_0) {
+        current_button = current_button >> 1;
+        if (current_button < ELEMENT_0) {
+            current_button = ELEMENT_7;
+        }
+    }
+    
+    // Left press
+    if (press & ELEMENT_2) {
+        current_button = current_button << 1;
+        if (current_button > ELEMENT_7) {
+            current_button = ELEMENT_0;
+        }
+    }
+    
+    // Update LEDS
+    pioc->sodr = current_button;
+    pioc->codr = ~current_button;
 }
 
-
-__int_handler *abdac_handler(void)
-{
-	dac->CR.en = 1;
+void abdac_isr(void) {
+    dac->CR.en = 1;
 }
-
 
 
--- oeving2/oeving2.h
+++ oeving2/oeving2.h
@@ -6,23 +6,35 @@
  *
  *****************************************************************************/
 
-#ifndef OEVING_2_H /* vanlig måte å unngå headerrekursjon på */
+#ifndef OEVING_2_H
 #define OEVING_2_H
 
-#include <avr32/ap7000.h> /* inkludere Atmel-headerfil */
+/* Include Atmel header files */
+#include <avr32/ap7000.h>
 #include <sys/interrupts.h>
 
-/* legg andre "includes" her, sammen med "defines" og prototyper */
+/* Interrupt priority levels */
 #define ABDAC_INT_LEVEL 0
 #define BUTTONS_INT_LEVEL 0
 
-/* prototyper */
+/* Convenient references for buttons and LEDs */
+#define ELEMENT_0 0x01
+#define ELEMENT_1 0x02
+#define ELEMENT_2 0x04
+#define ELEMENT_3 0x08
+#define ELEMENT_4 0x10
+#define ELEMENT_5 0x20
+#define ELEMENT_6 0x40
+#define ELEMENT_7 0x80
+#define ELEMENT_ALL 0xFF
+
+/* Prototypes */
 int main (int argc, char *argv[]);
+static void initHardware (void);
 static void initIntc(void);
 static void initButtons(void);
 static void initLeds(void);
 static void initAudio(void);
-static void initHardware (void);
 
 static void button_isr(void);
 static void abdac_isr(void);
@@ -39,17 +51,11 @@ ACTIVATED_BUTTONS is supposed to denote the following:
 #define ACTIVATED_BUTTONS 0xfc
 
 
-//__int_handler *int_handler(void);
-__int_handler *abdac_handler(void);//I'm thinking this might be a good thing
-__int_handler *button_handler(void);
-
-
-//Struct pointers given in the compendium(3.2.4 p.44)
-//pointing to PIO B and C, Power manager and ABDAC
-volatile avr32_dac_t *dac = &AVR32_DAC;
-volatile avr32_pio_t *pioc = &AVR32_PIOC;
+/* Convenient pointers to I/O devices */
+volatile avr32_abdac_t *dac = &AVR32_ABDAC;
 volatile avr32_pio_t *piob = &AVR32_PIOB;
-volatile avr32_sm_t *sm = &AVR32_SM;
+volatile avr32_pio_t *pioc = &AVR32_PIOC;
+volatile avr32_pm_t *sm = &AVR32_PM;
 
 
 #endif
