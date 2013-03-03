c23909b763e5f3b328a677191838fde0033250ef
Per Thomas Lundal <perthomas@gmail.com>
2013-03-03 19:16:47 +0100

    Midi parser complete. Now to merge the tracks and store it in a struct...

--- oeving2/midi/midi_to_struct.c
+++ oeving2/midi/midi_to_struct.c
@@ -6,28 +6,181 @@
 int main (int argc, char *argv[]) {
     buffer_t* buffer = read_file("test.mid");
     
+    /*
     int i;
     for (i = 0; i < buffer->length; i++) {
         printf("%02X ", (int)buffer->data[i]);
     }
+    */
+    
+    parse_midi(buffer);
     
     return 0;
 }
 
-int parse_int(buffer_t *buffer) {
+void parse_midi(buffer_t *buffer) {
+    // Read data
+    int id = parse_int(buffer, 4);
+    int length = parse_int(buffer, 4);
+    int format = parse_int(buffer, 2);
+    int tracks = parse_int(buffer, 2);
+    int division = parse_int(buffer, 2);
+    
+    // TODO: Set sample_points per tick
+    printf("Midi - Format: %d, Tracks: %d, Division: %d\n", format, tracks, division);
+    
+    // Parse tracks
+    int i;
+    for (i = 0; i < tracks; i++) {
+        parse_track(buffer);
+    }
+    
+    printf("Midi - End\n");
+}
+
+void parse_track(buffer_t *buffer) {
+    // Read data
+    int id = parse_int(buffer, 4);
+    int length = parse_int(buffer, 4);
+    
+    printf("Track - Length: %d\n", length);
+    
+    // Parse events
+    int data_end = buffer->position + length;
+    while (buffer->position < data_end) {
+        parse_event(buffer); 
+    }
+    
+    printf("Track - End\n");
+}
+
+void parse_event(buffer_t *buffer) {
+    // Read data
+    int delta_time = parse_varint(buffer);
+    int first_byte = parse_int(buffer, 1);
+    int type = (first_byte & 0xF0) >> 4;
+    int channel = first_byte & 0x0F;
+    
+    switch (type) {
+        // Note off
+        case 0x8: {
+            // Read data
+            int note = parse_int(buffer, 1);
+            int velocity = parse_int(buffer, 1);
+            
+            // TODO
+            printf("Delta %d, Channel %d, Note %d off (%d)\n", delta_time, channel, note, velocity);
+            break;
+        }
+        // Note on
+        case 0x9: {
+            // Read data
+            int note = parse_int(buffer, 1);
+            int velocity = parse_int(buffer, 1);
+            
+            // TODO
+            printf("Delta %d, Channel %d, Note %d on (%d)\n", delta_time, channel, note, velocity);
+            break;
+        }
+        // Note aftertouch
+        case 0xA: {
+            // Read data
+            int note = parse_int(buffer, 1);
+            int value = parse_int(buffer, 1);
+            
+            // Skip
+            break;
+        }
+        // Controller
+        case 0xB: {
+            // Read data
+            int number = parse_int(buffer, 1);
+            int data = parse_int(buffer, 1);
+            
+            // Skip
+            break;
+        }
+        // Program change
+        case 0xC: {
+            // Read data
+            int number = parse_int(buffer, 1);
+            
+            // Skip
+            break;
+        }
+        // Channel aftertouch
+        case 0xD: {
+            // Read data
+            int value = parse_int(buffer, 1);
+            
+            // Skip
+            break;
+        }
+        // Pitch bend
+        case 0xE: {
+            // Read data
+            int low_val = parse_int(buffer, 1);
+            int high_val = parse_int(buffer, 1);
+            
+            // Skip
+            break;
+        }
+        // SysEx or Meta event
+        case 0xF: {
+            // SysEx event
+            if (channel == 0x0 || channel == 0x7) {
+                // Read data
+                int sysex_length = parse_varint(buffer);
+                
+                // Skip rest
+                buffer->position += sysex_length;
+            }
+            // Meta event
+            if (channel == 0xF) {
+                // Read data
+                int meta_type = parse_int(buffer, 1);
+                int meta_length = parse_varint(buffer);
+                
+                //TODO: Parse Set Tempo?
+                
+                // Skip rest
+                buffer->position += meta_length;
+            }
+            break;
+        }
+    }
+}
+
+int parse_int(buffer_t *buffer, int num_bytes) {
     int value = 0;
+    
+    int i;
+    for (i = 0; i < num_bytes; i++) {
+        // Read byte
+        int data = buffer->data[buffer->position++];
+        
+        // Shift previous and add current
+        value = (value << 8) + data;
+    }
+    
+    return value;
+}
+
+int parse_varint(buffer_t *buffer) {
+    int value = 0;
+    
     while (1) {
         // Read byte
-        char data = buffer->data[buffer->position++];
+        int data = buffer->data[buffer->position++];
         
         // Mask for the first bit
-        char mask = 1 << 7;
+        int mask = 1 << 7;
         
         // Shift previous and add current
         value = (value << 7) + (data & ~mask);
         
         // If MSD is 0: break
-        if (data & mask) {
+        if ((data & mask) == 0) {
             break;
         }
     }
--- oeving2/midi/midi_to_struct.h
+++ oeving2/midi/midi_to_struct.h
@@ -1,11 +1,15 @@
 
 typedef struct {
-    char *data;
+    unsigned char *data;
     long length;
     long position;
 } buffer_t;
 
 // Prototypes
 int main (int argc, char *argv[]);
-int parse_int(buffer_t *buffer);
+void parse_midi(buffer_t *buffer);
+void parse_track(buffer_t *buffer);
+void parse_event(buffer_t *buffer);
+int parse_int(buffer_t *buffer, int num_bytes);
+int parse_varint(buffer_t *buffer);
 buffer_t* read_file(char *name);
