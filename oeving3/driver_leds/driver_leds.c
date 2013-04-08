c072311eeffcd2d74720554e59e57647afa113a7
Per Thomas Lundal <perthomas@gmail.com>
2013-04-09 01:26:18 +0200

    Implemented write. Use A..H to turn on and a..h to turn off. Register in /dev by using major/minor numbers from dmesg

--- oeving3/driver_leds/driver_leds.c
+++ oeving3/driver_leds/driver_leds.c
@@ -38,6 +38,7 @@ static struct file_operations driver_fops = {
 // Constants
 
 static dev_t dev;
+static struct cdev *cdev;
 static unsigned long MEM_ADDR_START = AVR32_PIOB_ADDRESS;
 static unsigned long MEM_ADDR_LEN = AVR32_PIOC_ADDRESS - AVR32_PIOB_ADDRESS;
 
@@ -52,8 +53,8 @@ static int __init driver_init(void) {
 		+ elements[4] + elements[5] + elements[6] + elements[7];
 	
 	// Allocate device number
-	dev = MKDEV(1337, 0);
-	int res_devnum = register_chrdev_region(dev, 1, "LEDS");
+	//dev = MKDEV(1337, 0);
+	int res_devnum = alloc_chrdev_region(&dev, 0, 1, "LEDS");
 	
 	// Check for errors
 	if (res_devnum < 0) return res_devnum;
@@ -65,13 +66,13 @@ static int __init driver_init(void) {
 	AVR32_PIOB.per = elements_all;
 	AVR32_PIOB.oer = elements_all;
 	AVR32_PIOB.codr = elements_all;
-	AVR32_PIOB.sodr = elements[1] + elements[3] + elements[5];
 	
 	// Register Device
-	struct cdev *cdev = cdev_alloc();
+	cdev = cdev_alloc();
 	cdev_init(cdev, &driver_fops);
+	cdev_add(cdev, dev, 1);
 	
-	printk(KERN_INFO "LED driver loaded!");
+	printk(KERN_INFO "LED driver loaded! (%d, %d)", MAJOR(dev), MINOR(dev));
 	
 	return 0;
 }
@@ -82,6 +83,7 @@ static void __exit driver_exit(void) {
 	// TODO?
 	
 	// Release device number
+	cdev_del(cdev);
 	unregister_chrdev_region(dev, 1);
 	
 	// Release hardware
@@ -105,11 +107,60 @@ static int driver_release(struct inode *inode, struct file *filp) {
 }
 
 static ssize_t driver_read(struct file *filp, char __user *buff, size_t count, loff_t *offp) {
+	/*int pindata = AVR32_PIOB.pdsr;
+	char data[8];
+	
+	data[0] = (pindata & elements[0]) ? 'A' : 'a';
+	data[1] = (pindata & elements[1]) ? 'B' : 'b';
+	data[2] = (pindata & elements[2]) ? 'C' : 'c';
+	data[3] = (pindata & elements[3]) ? 'D' : 'd';
+	data[4] = (pindata & elements[4]) ? 'E' : 'e';
+	data[5] = (pindata & elements[5]) ? 'F' : 'f';
+	data[6] = (pindata & elements[6]) ? 'G' : 'g';
+	data[7] = (pindata & elements[7]) ? 'H' : 'h';
+	
+	/
+	int i = count;
+	while (i--) {
+		
+	}
+	/
+	
+	copy_to_user(buff, data, count);
+	
+	return count;*/
 	return 0;
 }
 
 static ssize_t driver_write(struct file *filp, const char __user *buff, size_t count, loff_t *offp) {
-	return 0;
+	char data[count];
+	copy_from_user(data, buff, count);
+	
+	int i = 0;
+	while (i < count) {
+		switch (data[i]) {
+			case 'A': AVR32_PIOB.SODR.p8 = 1; break;
+			case 'B': AVR32_PIOB.SODR.p9 = 1; break;
+			case 'C': AVR32_PIOB.SODR.p10 = 1; break;
+			case 'D': AVR32_PIOB.SODR.p13 = 1; break;
+			case 'E': AVR32_PIOB.SODR.p14 = 1; break;
+			case 'F': AVR32_PIOB.SODR.p15 = 1; break;
+			case 'G': AVR32_PIOB.SODR.p16 = 1; break;
+			case 'H': AVR32_PIOB.SODR.p30 = 1; break;
+			
+			case 'a': AVR32_PIOB.CODR.p8 = 1; break;
+			case 'b': AVR32_PIOB.CODR.p9 = 1; break;
+			case 'c': AVR32_PIOB.CODR.p10 = 1; break;
+			case 'd': AVR32_PIOB.CODR.p13 = 1; break;
+			case 'e': AVR32_PIOB.CODR.p14 = 1; break;
+			case 'f': AVR32_PIOB.CODR.p15 = 1; break;
+			case 'g': AVR32_PIOB.CODR.p16 = 1; break;
+			case 'h': AVR32_PIOB.CODR.p30 = 1; break;
+		}
+		i++;
+	}
+	
+	return count;
 }
 
 /*****************************************************************************/
