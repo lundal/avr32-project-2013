4f5246a8f2bd0ecad2f1d3633d9d5bf3ac4b956c
Per Thomas Lundal <perthomas@gmail.com>
2013-04-23 12:06:55 +0200

    Create program to show ip at bootup

---
+++ oeving3/ip.c
@@ -0,0 +1,28 @@
+#include "graphics.h"
+#include "fonts.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Read ip address
+    FILE *ip_file;
+    ip_file = popen("ifconfig | grep 'Bcast' | awk '{print $2}'", "r");
+    char ip[100];
+    fgets(data, 100, ip_file);
+    pclose(ip_file);
+    
+    // Init screen
+    screen_init();
+    
+    // Load font
+    font *f = font_load("font_small");
+    
+    // Print ip
+    screen_draw_text(10,10, f, ip);
+    
+    // Dispose screen
+    screen_dispose();
+    
+    return 0;
+}
