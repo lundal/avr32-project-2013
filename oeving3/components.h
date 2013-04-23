ac11e61b1698300d4615af9888738c8f55ecc458
Per Thomas Lundal <perthomas@gmail.com>
2013-04-23 21:36:27 +0200

    Remove unneeded prototypes from components.h

--- oeving3/components.h
+++ oeving3/components.h
@@ -3,29 +3,11 @@
 
 #include "component.h"
 
-// Controllable component
+// Components
 extern component *component_upup;
-void component_upup_add(int component_nr, gameobject *object, void *param);
-void component_upup_tick(int component_nr, gameobject *object, void *param);
-void component_upup_remove(int component_nr, gameobject *object, void *param);
-
-// Sprite component
 extern component *component_sprite;
-void component_sprite_add(int component_nr, gameobject *object, void *param);
-void component_sprite_tick(int component_nr, gameobject *object, void *param);
-void component_sprite_remove(int component_nr, gameobject *object, void *param);
-
-// Player control component
 extern component *component_player_control;
-void component_player_control_add(int component_nr, gameobject *object, void *param);
-void component_player_control_tick(int component_nr, gameobject *object, void *param);
-void component_player_control_remove(int component_nr, gameobject *object, void *param);
-
-// Shooting component
 extern component *component_shoot;
-void component_shoot_add(int component_nr, gameobject *object, void *param);
-void component_shoot_tick(int component_nr, gameobject *object, void *param);
-void component_shoot_remove(int component_nr, gameobject *object, void *param);
 
 // Init function
 void components_init();
