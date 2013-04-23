9d8d8fc9c66a308dde6f71a306221fb9c3c609d6
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2013-04-23 18:31:35 +0200

    added a test of player_control_component

--- oeving3/engine_test.c
+++ oeving3/engine_test.c
@@ -8,16 +8,23 @@ int main() {
     engine_init();
     
     // Init components
-    component_example = component_create(&component_example_add, &component_example_tick, component_example_remove);
-    component_controllable = component_create(&component_controllable_add, &component_controllable_tick, component_controllable_remove);
-    component_sprite = component_create(&component_sprite_add, &component_sprite_tick, component_sprite_remove);
+    component_example = component_create(&component_example_add, &component_example_tick, &component_example_remove);
+    component_player_control = component_create(&component_player_control_add, &component_player_control_tick, &component_player_control_remove);
+    component_sprite = component_create(&component_sprite_add, &component_sprite_tick, &component_sprite_remove);
     
     // Add object
-    gameobject *rabbit = gameobject_create();
-    component_add(rabbit, component_controllable, NULL);
-    component_add(rabbit, component_sprite, "rabbit.bmp");
-    engine_gameobject_add(rabbit);
-    
+    gameobject *player1 = gameobject_create();
+    player1->pos_y = 150;
+    component_add(player1, component_player_control,(void*) 0);
+    component_add(player1, component_sprite, "rabbit.bmp");
+    engine_gameobject_add(player1);
+
+    gameobject *player2 = gameobject_create();
+    player2->pos_y = 150;
+    component_add(player2, component_player_control,(void*) 1);
+    component_add(player2, component_sprite, "rabbit.bmp");
+    engine_gameobject_add(player2);
+
     engine_run();
     
     return 0;
